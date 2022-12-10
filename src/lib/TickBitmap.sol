8bb07b42e31957971ab6913dddcb4676147d9516
Darko Kolev <kolevdarko@gmail.com>
2022-12-10 16:43:00 +0100

    next available tick one word away

--- src/lib/TickBitmap.sol
+++ src/lib/TickBitmap.sol
@@ -21,4 +21,37 @@ library TickBitmap {
         uint256 mask = 1 << bitPos;
         self[wordPos] ^= mask;
     }
+
+    function nextInitializedTickWithinOneWord(
+        mapping(int16 => uint256) storage self,
+        int24 tick,
+        int24 tickSpacing,
+        bool lte
+    ) internal view returns (int24 next, bool initialized) {
+        int24 compressed = tick / tickSpacing;
+        if (tick < 0 && tick % tickSpacing != 0) compressed--;
+        if (lte) {
+            (int16 wordPos, uint8 bitPos) = position(compressed);
+            uint256 mask = (1 << bitPos) - 1 + (1 << bitPos);
+            uint256 masked = self[wordPos] & mask;
+
+            initialized = masked != 0;
+            next = initialized
+                ? (compressed -
+                    int24(
+                        uint24(bitPos - BitMath.mostSignificantBit(masked))
+                    )) * tickSpacing
+                : (compressed - int24(uint24(bitPos))) * tickSpacing;
+        } else {
+            (int16 wordPos, uint8 bitPos) = position(compressed + 1);
+            // get all ones to the left of bitPos inclusive
+            uint256 mask = ~((1 << bitPos) - 1);
+            uint256 masked = self[wordPos] & mask;
+            // if there are no initialized ticks to the left of the current tick, return leftmost in the word
+            initialized = masked != 0;
+            next = initialized 
+            ? (compressed + 1 + int24(uint24((BitMath.leastSignificantBit(masked) - bitPos)))) * tickSpacing;
+            : (compressed + 1 + int24(uint24((type(uint8).max - bitPos)))) * tickSpacing;
+        }
+    }
 }
